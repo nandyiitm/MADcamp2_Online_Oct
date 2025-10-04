f15814eb6d67ee3dc90f31af858ebe0ee221efc6
nandy <22f3002857@ds.study.iitm.ac.in>
2025-10-04 19:37:36 +0530

    updated requirements

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from flask_cors import CORS
 
@@ -15,7 +15,29 @@ class HelloWorld(Resource):
     def get(self):
         return {'message': 'Bye world!'}
 
+users = []    
+
+class UserRegister(Resource):
+    def get(self):
+        username = request.args.get('username')
+        if not username:
+            return {'message': 'Username is required'}, 400
+        print(f"Registering user: {username}")
+        users.append(username)
+        return {'message': 'User registered successfully'}, 201
+    
+class UserLogin(Resource):
+    def get(self):
+        return {'message': 'User logged in successfully'}, 200
+    
+class UsersInfo(Resource):
+    def get(self):
+        return {'users': users}, 200
+
 api.add_resource(HelloWorld, '/')
+api.add_resource(UserRegister, '/register')
+api.add_resource(UserLogin, '/login')
+api.add_resource(UsersInfo, '/users')
 
 if __name__ == '__main__':
     app.run(debug=True)
